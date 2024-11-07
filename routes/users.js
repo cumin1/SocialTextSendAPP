d1798e8641c93b385c54ceb9301ffe1c948bbd5e
Fuhao <113490377+cumin1@users.noreply.github.com>
2024-11-07 17:25:49 +0800

    实现用户登录接口
    登录成功之后给令牌 遇到问题：如果不使用异步函数 数据可能查询不到

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const User = require('../models/User')
+const jwt = require('jsonwebtoken')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -8,30 +9,83 @@ router.get('/', function(req, res, next) {
 });
 
 // 用户注册接口
-router.post('/register',function(req,res,next){
-    const {username,email,password} = req.body;
-    
-    // 注册用户
-    const newUser = new User({
-      username: username,
-      email: email,
-      password: password
+router.post('/register', function (req, res, next) {
+  const { username, email, password } = req.body;
+
+  // 注册用户
+  const newUser = new User({
+    username: username,
+    email: email,
+    password: password
+  })
+
+  try {
+    newUser.save();
+    res.status(201).json({
+      status: "success",
+      message: "用户注册成功!",
+      data: newUser
     })
+  } catch (err) {
+    res.status(400).json({
+      status: "error",
+      message: err.message
+    })
+  }
+
+})
+
+// 用户登录接口
+router.post('/login', async function(req, res, next) {
+    const { email, password } = req.body;
 
     try {
-      newUser.save();
-      res.status(201).json({
-        status: "success",
-        message: "用户注册成功!",
-        data: newUser
-      })
+        // 查询用户
+        const user = await User.findOne({ email: email }); // 使用await
+
+        // 当没有找到用户的逻辑
+        if (!user) {
+            return res.status(400).json({
+                status: "error",
+                message: "用户未找到"
+            });
+        }
+
+        // 打印用户信息
+        console.log("email: ", user.email);
+        console.log("password: ", user.password);
+        console.log("type of password is", typeof(password));
+        console.log("type of user.password is", typeof(user.password));
+
+        // 检查密码
+        if (user.password !== password) {
+            return res.status(401).json({
+                status: "error",
+                message: "密码错误"
+            });
+        }
+
+        // 生成JWT
+        const token = jwt.sign({ id: user._id, email: user.email }, '123456', { expiresIn: '1h' });
+        
+        return res.status(200).json({
+            status: "success",
+            massage: "登录成功",
+            data: {
+                user: {
+                    id: user._id,
+                    email: user.email,
+                    username: user.username
+                },
+                token: token // 返回生成的令牌
+            }
+        });
     } catch (err) {
-      res.status(400).json({
-        status: "error",
-        message: err.message
-      })
+        res.status(500).json({
+            status: "error",
+            message: err.message
+        });
     }
-    
-})
+});
 
 module.exports = router;
